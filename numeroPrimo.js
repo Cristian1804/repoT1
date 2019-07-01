5b061c146cf240fbf15f1b5a61382053134b65a0
Cristian Garrido <garridocristian1804@gmail.com>
2019-07-01 17:28:01 -0300

    commit

--- numeroPrimo.js
+++ numeroPrimo.js
@@ -1,1 +1,8 @@
-"use strit"
+"use strict"
+let readlineSync = require('readline-sync');
+let isPrime = require('is-prime-number');
+const number = readlineSync.questionInt('Es primo el numero ');
+
+console.log(number);
+ 
+console.log( isPrime(number) );
